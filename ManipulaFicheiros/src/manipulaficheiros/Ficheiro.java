d9d22d4c624763ead49967859097c8484020d1a3
Joao Nogueira <jnprogcencal@gmail.com>
2022-10-03 15:23:26 +0100

    Criar ficheiros

--- ManipulaFicheiros/src/manipulaficheiros/Ficheiro.java
+++ ManipulaFicheiros/src/manipulaficheiros/Ficheiro.java
@@ -1,11 +1,26 @@
 package manipulaficheiros;
 
 import java.io.File;
+import java.io.IOException;
+import java.util.logging.Level;
+import java.util.logging.Logger;
 
 class Ficheiro {
 
     static void criaFicheiro() {
-
+        String nome;
+        System.out.println("\n\nInsira o nome do ficheiro a criar");        
+        nome = ManipulaFicheiros.ler.nextLine();
+        nome = ManipulaFicheiros.ler.next();
+        File ficheiro = new File(nome);
+        if(!ficheiro.exists()){
+            try {
+                ficheiro.createNewFile();
+            } catch (IOException ex) {
+                ex.printStackTrace();
+            }
+        }
+        System.out.println("\n");
     }
 
     static void verificaSeExiste() {
