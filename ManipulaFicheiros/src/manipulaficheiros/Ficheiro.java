2ab673cc384d9cbd0ebfc69b41e513effd4a96d1
Joao Nogueira <jnprogcencal@gmail.com>
2022-10-03 15:00:40 +0100

    Mostra lista ficheiros

--- ManipulaFicheiros/src/manipulaficheiros/Ficheiro.java
+++ ManipulaFicheiros/src/manipulaficheiros/Ficheiro.java
@@ -1,5 +1,7 @@
 package manipulaficheiros;
 
+import java.io.File;
+
 class Ficheiro {
 
     static void criaFicheiro() {
@@ -23,7 +25,14 @@ class Ficheiro {
     }
 
     static void mostraListaFicheiros() {
-
+        File ficheiro = new File("C:\\Users\\jnogueira\\Desktop\\0816 - Java\\Ficheiros");
+        File [] lista = ficheiro.listFiles();
+        System.out.println("\n\n>>> Lista de Ficheiros <<<");
+        for(int x=0;x<lista.length;x++){
+            System.out.println(lista[x].getName());
+        }
+        System.out.println("\n");
+       
     }
     
 }
